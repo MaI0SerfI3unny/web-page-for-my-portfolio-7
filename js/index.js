969c115c2e749743d3983c91eb41c85d3188a194
ProggerOnGod <daniledkin@gmail.com>
2020-10-14 16:57:31 +0300

    60%

---
+++ js/index.js
@@ -0,0 +1,120 @@
+ function i1a() {
+        var i1a = document.getElementById("i1a");
+        var i2a = document.getElementById("i2a");
+        var i3a = document.getElementById("i3a");
+        var i4a = document.getElementById("i4a");
+
+          i1a.style.backgroundColor = "#ba1fa0";
+          i1a.style.color ="white";
+          i2a.style.backgroundColor = "white";
+          i2a.style.color ="#858585";
+          i3a.style.backgroundColor = "white";
+          i3a.style.color ="#858585";
+          i4a.style.backgroundColor = "white";
+          i4a.style.color ="#858585";
+}
+
+function i2a() {
+  var i1a = document.getElementById("i1a");
+  var i2a = document.getElementById("i2a");
+  var i3a = document.getElementById("i3a");
+  var i4a = document.getElementById("i4a");
+         i2a.style.backgroundColor = "#ba1fa0";
+         i2a.style.color ="white";
+         i1a.style.backgroundColor = "white";
+         i1a.style.color ="#858585";
+         i3a.style.backgroundColor = "white";
+         i3a.style.color ="#858585";
+         i4a.style.backgroundColor = "white";
+         i4a.style.color ="#858585";
+
+}
+
+function i3a() {
+  var i1a = document.getElementById("i1a");
+  var i2a = document.getElementById("i2a");
+  var i3a = document.getElementById("i3a");
+  var i4a = document.getElementById("i4a");
+         i3a.style.backgroundColor = "#ba1fa0";
+         i3a.style.color ="white";
+         i1a.style.backgroundColor = "white";
+         i1a.style.color ="#858585";
+         i2a.style.backgroundColor = "white";
+         i2a.style.color ="#858585";
+         i4a.style.backgroundColor = "white";
+         i4a.style.color ="#858585";
+}
+
+function i4a() {
+  var i1a = document.getElementById("i1a");
+  var i2a = document.getElementById("i2a");
+  var i3a = document.getElementById("i3a");
+  var i4a = document.getElementById("i4a");
+         i4a.style.backgroundColor = "#ba1fa0";
+         i4a.style.color ="white";
+         i1a.style.backgroundColor = "white";
+         i1a.style.color ="#858585";
+         i2a.style.backgroundColor = "white";
+         i2a.style.color ="#858585";
+         i3a.style.backgroundColor = "white";
+         i3a.style.color ="#858585";
+}
+
+function card(){
+  var top = document.getElementById("top");
+  var top = document.getElementById("card");
+  var mainpricing = document.getElementById("main-pricing");
+
+  top.style.backgroundColor = "#dd2ec0";
+  mainpricing.style.backgroundColor = "#ba1fa0";
+}
+
+function carddown(){
+  var top = document.getElementById("top");
+  var top = document.getElementById("card");
+  var mainpricing = document.getElementById("main-pricing");
+
+  top.style.backgroundColor = "white";
+  mainpricing.style.backgroundColor = "white";
+}
+
+function card1(){
+  var top = document.getElementById("top1");
+  var top = document.getElementById("card1");
+  var mainpricing = document.getElementById("main-pricing1");
+
+  top.style.backgroundColor = "#dd2ec0";
+  mainpricing.style.backgroundColor = "#ba1fa0";
+}
+
+function carddown1(){
+  var top1 = document.getElementById("top1");
+  var top1 = document.getElementById("card1");
+  var mainpricing1 = document.getElementById("main-pricing1");
+
+  top1.style.backgroundColor = "white";
+  mainpricing1.style.backgroundColor = "white";
+}
+
+function card2(){
+  var top2 = document.getElementById("top2");
+  var top2 = document.getElementById("card2");
+  var mainpricing2 = document.getElementById("main-pricing2");
+  var btn2 = document.getElementById("btn2");
+
+
+  top2.style.backgroundColor = "#dd2ec0";
+  mainpricing2.style.backgroundColor = "#ba1fa0";
+  btn2.style.backgroundColor = "#ffccff";
+}
+
+function carddown2(){
+  var top2 = document.getElementById("top2");
+  var top2 = document.getElementById("card2");
+  var mainpricing2 = document.getElementById("main-pricing2");
+  var btn2 = document.getElementById("btn2");
+
+  top2.style.backgroundColor = "white";
+  mainpricing2.style.backgroundColor = "white";
+  btn2.style.backgroundColor = "#ba1fa0";
+}
